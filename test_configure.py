4b97203a55344809225fa394e63ab3a2c39466b1
Jason Chambers <jason.d.chambers@gmail.com>
2022-07-14 12:19:06 -0400

    Avoid over-writing ~/.netorg.cfg during unit test

--- test_configure.py
+++ test_configure.py
@@ -1,5 +1,6 @@
 """Tests for NetorgConfigurator."""
 
+import os
 import unittest
 from unittest.mock import patch
 from configure import NetorgConfigurator
@@ -7,8 +8,9 @@ from configure import NetorgConfigurator
 class NetorgConfiguratorTest(unittest.TestCase) :
     """Tests for NetorgConfigurator"""
 
+    @patch('configure.NetorgConfigurator.get_config_filename')
     @patch('configure.NetorgConfigurator.get_config')
-    def test_save_and_load(self, mock_get_config):
+    def test_save_and_load(self, mock_get_config, mock_get_config_filename):
         """Test NetorgConfigurator.save() and load()"""
         netorg_configurator = NetorgConfigurator()
         mock_get_config.return_value = {
@@ -20,9 +22,10 @@ class NetorgConfiguratorTest(unittest.TestCase) :
             "vlan_id": 1,
             "vlan_subnet": "192.168.128.0/24"
         }
-        # TODO - take a backup copy of any existing config
+        temp_config_file = ".tmp.netorg.cfg.for_testing"
+        mock_get_config_filename.return_value = temp_config_file
         netorg_configurator.save()
         netorg_configurator2 = NetorgConfigurator()
         netorg_configurator2.load()
-        # TODO - Restore backup copy of any existing config
         self.assertDictEqual(mock_get_config.return_value,netorg_configurator2.config)
+        os.remove(temp_config_file)
