834038decfd91d1fd273813c13ea945fa69baac9
Jason Chambers <jason.d.chambers@gmail.com>
2022-07-18 16:47:17 -0400

    lint netorg.py

--- netorg.py
+++ netorg.py
@@ -1,17 +1,19 @@
 """This is the main module for Netorg."""
 import argparse
 import sys
-import merakidevicetableloaderfactory 
+import merakidevicetableloaderfactory
 from configure import NetorgConfigurator
-from netorgmeraki import MerakiNetworkMapper, MerakiWrapper
+from netorgmeraki import MerakiNetworkMapper
 from scan import NetorgScanner
 from generate import NetorgGenerator
 
 def load_device_table(config):
+    """Load the device table."""
     device_table_loader = merakidevicetableloaderfactory.create(config)
     return device_table_loader.load_all()
 
 def load_config():
+    """Load the configuratil file."""
     configurator = NetorgConfigurator()
     configurator.load()
     return configurator.get_config()
@@ -20,11 +22,16 @@ def main():
     """Figure out what the user wants to happen and make it so."""
     parser = argparse.ArgumentParser(description='Organize your network.')
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-c", "--configure", help="[Re-]Configure Netorg", action="store_true")
-    group.add_argument("-g", "--generate", help="Generate/update a devices.yml", action="store_true")
-    group.add_argument("-s", "--scan", help="Scan to see what's active, known, reserved", action="store_true")
-    group.add_argument("-o", "--organize", help="Organize the network", action="store_true")
-    group.add_argument("-d", "--devicetable", help="Export the device table", action="store_true")
+    group.add_argument("-c", "--configure", help="[Re-]Configure Netorg",
+                       action="store_true")
+    group.add_argument("-g", "--generate", help="Generate/update a devices.yml",
+                       action="store_true")
+    group.add_argument("-s", "--scan", help="Scan to see what's active, known, reserved",
+                       action="store_true")
+    group.add_argument("-o", "--organize", help="Organize the network",
+                       action="store_true")
+    group.add_argument("-d", "--devicetable", help="Export the device table",
+                       action="store_true")
     args = parser.parse_args()
     if args.configure:
         print("Configure")
@@ -54,8 +61,6 @@ def main():
         meraki_network_mapper = MerakiNetworkMapper(config, device_table)
         meraki_network_mapper.make_fixed_ip_reservations()
     elif args.devicetable:
-        #TODO allow a DataFrame query string to be passed
-        #TODO allow the columns to be specified
         print("Export the device table")
         config = load_config()
         device_table = load_device_table(config)
