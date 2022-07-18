db6092c9e08f225b91e742482eacb65ad59e0ede
Jason Chambers <jason.d.chambers@gmail.com>
2022-07-18 10:50:40 -0400

    pylint clean up for devicetable.py

--- devicetable.py
+++ devicetable.py
@@ -1,104 +1,113 @@
+"""All the things associated with Loading, building and accessing a device table."""
 import pandas as pd
 from pandas import DataFrame
 
 class DeviceTable :
+    """The device table is the heart of Network Organizer."""
     def __init__(self,data) -> None:
+        # pylint: disable=invalid-name
         self.df = pd.DataFrame(data)
 
     def has_unique_macs(self) -> bool:
+        """Return True if the device table has unique MACs."""
         if len(self.df.mac.unique() == self.df.shape[0]) :
-            return True 
-        else :
-            print('Duplicate MACs in device table')
-            return False
+            return True
+        print('Duplicate MACs in device table')
+        return False
 
     def has_unique_ips(self) -> bool:
+        """Return True if the device table has unique IP addresses."""
         if len(self.df.ip.unique() == self.df.shape[0]) :
-            return True 
-        else :
-            print('Duplicate IPs in device table')
-            return False
+            return True
+        print('Duplicate IPs in device table')
+        return False
 
 class DeviceTableBuilder :
+    """Efficiently Build a DeviceTable."""
     def __init__(self) -> None:
-        self.devices_dict = dict()
-        
+        self.devices_dict = {}
+
+    @staticmethod
     def generate_new_record() -> dict:
-        return { 
-            'known': False, 
-            'reserved': False, 
-            'active': False, 
-            'ip': '', 
-            'group': 'unclassified', 
+        """Generate a new record with default values."""
+        return {
+            'known': False,
+            'reserved': False,
+            'active': False,
+            'ip': '',
+            'group': 'unclassified',
             'name': ''}
 
     def get_details(self,mac) -> dict:
+        """For a given device identified by it's MAC, return the details."""
         if mac in self.devices_dict:
             return self.devices_dict[mac]
-        else:
-            self.devices_dict[mac] = {}
-            return self.devices_dict[mac]
-            
+        self.devices_dict[mac] = {}
+        return self.devices_dict[mac]
+
     def set_details(self,mac,details) -> None:
+        """Set the details for a given device identified by it's MAC."""
         self.devices_dict[mac] = details
-        
+
     def build(self) -> DeviceTable:
+        """Build the DeviceTable."""
         data = []
+        # pylint: disable=consider-using-dict-items
         for mac in self.devices_dict:
             details = self.devices_dict[mac]
-            s = {'mac': mac}
-            #data.append(s | details)
-            merged = {**s,**details}
+            device_mac_dict = {'mac': mac}
+            merged = {**device_mac_dict,**details}
             data.append(merged)
         return DeviceTable(data)
-        
-class DeviceTableLoaderException(Exception) :
-    pass
 
 class DeviceTableLoader :
+    """Load data into the DeviceTable."""
 
-    def __init__(self,known_devices_loader,active_clients_loader,fixed_ip_reservations_loader) -> None:
-        self.device_table_builder = DeviceTableBuilder() 
+    def __init__(self,known_devices_loader,active_clients_loader,
+                 fixed_ip_reservations_loader) -> None:
+        self.device_table_builder = DeviceTableBuilder()
         self.known_devices_loader = known_devices_loader
         self.active_clients_loader = active_clients_loader
         self.fixed_ip_reservations_loader = fixed_ip_reservations_loader
 
-
     def load_known(self) -> None:
+        """Load known devices into the DeviceTable."""
         known_devices = self.known_devices_loader.load()
         for device in known_devices :
             # All we know at this point is the device is known
             record = DeviceTableBuilder.generate_new_record()
             record['known'] = True
-            record['group'] = device['group'] 
+            record['group'] = device['group']
             record['name'] = device['name']
             self.device_table_builder.set_details(device['mac'], record)
 
-    def load_active_clients(self) : 
-        active_clients = self.active_clients_loader.load() 
+    def load_active_clients(self) :
+        """Load active clients into the DeviceTable."""
+        active_clients = self.active_clients_loader.load()
         for active_client in active_clients :
             record = self.device_table_builder.get_details(active_client['mac'])
             if record:
                 # device has been loaded already
                 record['active'] = True
-                record['ip'] = active_client['ip'] 
+                record['ip'] = active_client['ip']
                 self.device_table_builder.set_details(active_client['mac'], record)
             else:
                 # Seeing device for the first time
                 record = DeviceTableBuilder.generate_new_record()
-                record['active'] = True 
+                record['active'] = True
                 record['ip'] = active_client['ip']
                 record['name'] = active_client['description']
                 self.device_table_builder.set_details(active_client['mac'], record)
 
     def load_fixed_ip_reservations(self) :
+        """Load fixed IP reservations into the DeviceTable."""
         fixed_ip_reservations = self.fixed_ip_reservations_loader.load()
         if fixed_ip_reservations:
             for mac, fixed_ip_reservation_details in fixed_ip_reservations.items():
                 record = self.device_table_builder.get_details(mac)
                 if record:
                     record['reserved'] = True
-                    if (record['active'] == False) :
+                    if record['active'] is False :
                         # Is in-active - has no IP so use the reserved IP
                         record['ip'] = fixed_ip_reservation_details['ip']
                     self.device_table_builder.set_details(mac, record)
@@ -111,6 +120,7 @@ class DeviceTableLoader :
                     self.device_table_builder.set_details(mac, record)
 
     def load_all(self) -> DataFrame :
+        """Load everything into the DeviceTable."""
         self.load_known()
         self.load_active_clients()
         self.load_fixed_ip_reservations()
