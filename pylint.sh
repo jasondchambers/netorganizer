a4e52d27143869aa429fd3444cef30ceed538fd3
Jason Chambers <jason.d.chambers@gmail.com>
2022-07-18 18:10:29 -0400

    Ensure NETORG_HOME is set in pylint.sh

--- pylint.sh
+++ pylint.sh
@@ -1,2 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ -z "${NETORG_HOME}" ]]; then
+   echo "Please set NETORG_HOME environment variable before proceeding"
+   exit 1
+fi
+
 source .venv/bin/activate 
 pylint *.py
