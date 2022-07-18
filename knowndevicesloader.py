a9709037c80b41f2d66663161b7eaf9e5e3ffcb8
Jason Chambers <jason.d.chambers@gmail.com>
2022-07-18 16:31:06 -0400

    Lint knowndevicesloader.py

--- knowndevicesloader.py
+++ knowndevicesloader.py
@@ -1,9 +1,11 @@
+"""Module for loading known devices."""
+
 import os.path
 import yaml
 
 class KnownDevicesLoader:
     """Load the file containing known devices."""
-        
+
     def __init__(self,filename='./devices.yml') -> None:
         self.filename = filename
 
@@ -12,23 +14,27 @@ class KnownDevicesLoader:
         device_list = []
         if os.path.exists(self.filename) :
             print(f'Loading known devices from {self.filename}')
-            with open(self.filename) as f:
-                data = yaml.load(f, Loader=yaml.FullLoader)
-                devices = data['devices'] 
+            with open(self.filename, encoding='utf8') as known_devices_file:
+                data = yaml.load(known_devices_file, Loader=yaml.FullLoader)
+                devices = data['devices']
                 device_group_names = devices.keys()
                 for device_group_name in device_group_names:
                     devices_in_group = devices[device_group_name]
                     for device_in_group in devices_in_group:
-                        s = device_in_group.split(',')
-                        device_name = s[0]
-                        device_mac = s[1]
-                        device = {'name': device_name, 'mac': device_mac, 'group': device_group_name}
+                        device_in_group_str = device_in_group.split(',')
+                        device_name = device_in_group_str[0]
+                        device_mac = device_in_group_str[1]
+                        device = {
+                            'name': device_name,
+                            'mac': device_mac,
+                            'group': device_group_name}
                         device_list.append(device)
         else:
-            print(f'{self.filename} not found') 
+            print(f'{self.filename} not found')
         return device_list
 
     def load_from_string(self,string) -> list:
+        """Load known devices from a string containing the YAML (used for testing)."""
         device_list = []
         data = yaml.safe_load(string)
         devices = data['devices']
@@ -36,9 +42,12 @@ class KnownDevicesLoader:
         for device_group_name in device_group_names:
             devices_in_group = devices[device_group_name]
             for device_in_group in devices_in_group:
-                s = device_in_group.split(',')
-                device_name = s[0]
-                device_mac = s[1]
-                device = {'name': device_name, 'mac': device_mac, 'group': device_group_name}
+                device_in_group_str = device_in_group.split(',')
+                device_name = device_in_group_str[0]
+                device_mac = device_in_group_str[1]
+                device = {
+                    'name': device_name,
+                    'mac': device_mac,
+                    'group': device_group_name}
                 device_list.append(device)
         return device_list
