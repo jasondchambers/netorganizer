25dcf51a38e014ea75a8fe642b3be5002a175bd4
Jason Chambers <jason.d.chambers@gmail.com>
2022-07-21 07:29:24 -0400

    Complete tests for knowndevicesloader.py

--- knowndevicesloader.py
+++ knowndevicesloader.py
@@ -4,39 +4,30 @@ import os.path
 import yaml
 
 class KnownDevicesLoader:
-    """Load the file containing known devices."""
+    """Load known devices."""
 
     def __init__(self,filename='./devices.yml') -> None:
         self.filename = filename
 
     def load(self) -> list:
-        """Load the file containing known devices."""
+        """Load known devices from (YAML) file."""
         device_list = []
         if os.path.exists(self.filename) :
             print(f'Loading known devices from {self.filename}')
             with open(self.filename, encoding='utf8') as known_devices_file:
                 data = yaml.load(known_devices_file, Loader=yaml.FullLoader)
-                devices = data['devices']
-                device_group_names = devices.keys()
-                for device_group_name in device_group_names:
-                    devices_in_group = devices[device_group_name]
-                    for device_in_group in devices_in_group:
-                        device_in_group_str = device_in_group.split(',')
-                        device_name = device_in_group_str[0]
-                        device_mac = device_in_group_str[1]
-                        device = {
-                            'name': device_name,
-                            'mac': device_mac,
-                            'group': device_group_name}
-                        device_list.append(device)
+                return self.load_data(data)
         else:
             print(f'{self.filename} not found')
         return device_list
 
-    def load_from_string(self,string) -> list:
-        """Load known devices from a string containing the YAML (used for testing)."""
+    def load_data(self,data) -> list:
+        """Load known devices."""
+        if not isinstance(data,dict):
+            raise ValueError("Invalid YAML")
+        if 'devices' not in data.keys():
+            raise ValueError("Invalid YAML")
         device_list = []
-        data = yaml.safe_load(string)
         devices = data['devices']
         device_group_names = devices.keys()
         for device_group_name in device_group_names:
@@ -51,3 +42,8 @@ class KnownDevicesLoader:
                     'group': device_group_name}
                 device_list.append(device)
         return device_list
+
+    def load_from_string(self,string) -> list:
+        """Load known devices from (YAML) string."""
+        data = yaml.safe_load(string)
+        return self.load_data(data)
---
+++ test_knowndevicesloader.py
@@ -0,0 +1,72 @@
+"""Module for testing knowndevicesloader."""
+
+import unittest
+import tempfile
+from knowndevicesloader import KnownDevicesLoader
+
+class TestKnownDevicesLoader(unittest.TestCase):
+    """Tests for KnownDevicesLoader."""
+
+    @staticmethod
+    def generate_test_data() -> str:
+        """Generate well-formed YAML for testing."""
+        good_yaml = "devices:\n"
+        good_yaml += "  eero:\n"
+        good_yaml += "    - Eero Beacon Lady Pit,18:90:88:28:eb:5b\n"
+        good_yaml += "    - Eero Beacon Family Room,18:90:88:29:2b:5b\n"
+        good_yaml += "  kitchen_appliances:\n"
+        good_yaml += "    - Kitchen Appliances Fridge,68:a4:0e:2d:9a:91\n"
+        return good_yaml
+
+    def inspect_output(self,devices):
+        """Inspect the out output from test."""
+        self.assertEqual(3, len(devices))
+        for device in devices:
+            if device['name'] == 'Eero Beacon Lady Pit':
+                self.assertEqual(device['mac'], '18:90:88:28:eb:5b')
+                self.assertEqual(device['group'], 'eero')
+            elif device['name'] == 'Eero Beacon Family Room':
+                self.assertEqual(device['mac'], '18:90:88:29:2b:5b')
+                self.assertEqual(device['group'], 'eero')
+            elif device['name'] == 'Kitchen Appliances Fridge':
+                self.assertEqual(device['mac'], '68:a4:0e:2d:9a:91')
+                self.assertEqual(device['group'], 'kitchen_appliances')
+
+    def test_load_good_yaml(self):
+        """Test well-formed YAML load."""
+        known_devices_loader = KnownDevicesLoader()
+        devices = known_devices_loader.load_from_string(TestKnownDevicesLoader.generate_test_data())
+        self.inspect_output(devices)
+
+    def test_load_bad_yaml(self):
+        """Test invalid YAML load."""
+        bad_yaml = "Not YAML at all\n"
+        known_devices_loader = KnownDevicesLoader()
+        self.assertRaises(ValueError, known_devices_loader.load_from_string, bad_yaml)
+
+    def test_load_file_not_found(self):
+        """Test load with file that does not exist."""
+        known_devices_loader = KnownDevicesLoader(filename="/does/not/exist.yml")
+        devices = known_devices_loader.load()
+        self.assertEqual(len(devices), 0)
+
+    def test_load_good_yaml_from_file(self):
+        """Test well-formed YAML load from file."""
+        # pylint: disable=consider-using-with
+        temp_file = tempfile.NamedTemporaryFile()
+        with open(temp_file.name, 'w', encoding='utf8', newline='') as yaml_fp:
+            yaml_fp.write(TestKnownDevicesLoader.generate_test_data())
+            yaml_fp.close()
+            known_devices_loader = KnownDevicesLoader(filename=temp_file.name)
+            devices = known_devices_loader.load()
+            self.inspect_output(devices)
+
+    def test_load_bad_yaml_from_file(self):
+        """Test invalid YAML load from file."""
+        # pylint: disable=consider-using-with
+        temp_file = tempfile.NamedTemporaryFile()
+        with open(temp_file.name, 'w', encoding='utf8', newline='') as yaml_fp:
+            yaml_fp.write('Not YAML at all')
+            yaml_fp.close()
+            known_devices_loader = KnownDevicesLoader(filename=temp_file.name)
+            self.assertRaises(ValueError, known_devices_loader.load)
